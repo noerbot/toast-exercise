2ff7aed2d3b85b97a293be7c6380046910798661
noerbot <noah.j.jeffrey@gmail.com>
2022-05-01 14:05:17 -0500

    Test for Adding Liked Submission to List
    This tests the second requirement: Toasts that are "liked" are added to the list on the page.
    
    Had to move the first test into a separate Toast.test.js file because running two tests in the same suite was mounting and unmounting <App />, causing memory leak warnings.

--- src/LikedSubmissionList.test.js
+++ src/LikedSubmissionList.test.js
@@ -4,8 +4,10 @@ import App from './App.js'
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
+import { waitFor } from '@testing-library/react';
 
-test('When you click "New Submission" your app must respond and show a toast' , async () => {
+test('Toasts that are "liked" are added to the list on the page' , async () => {
+
   // render the application
   render(<App/>);
 
@@ -16,7 +18,19 @@ test('When you click "New Submission" your app must respond and show a toast' , 
   userEvent.click(button);
 
   // grab elements that will asynchronously appear
-  const toast = await screen.findByText('LIKE');
-  // use special jest-dom matchers like .toBeInTheDocument()
-  expect(toast).toBeInTheDocument();
+  const toastLikeButton = await screen.findByText('LIKE');
+  const submissionFullName = document.getElementsByClassName('MuiSnackbarContent-message')[0].textContent.split('\n')[0];
+  // simulate user clicking the "like" button
+  await waitFor(() => {
+    const emptyListMessage = screen.queryByText('Submissions you like will appear here.');
+    expect(emptyListMessage).toBeInTheDocument();
+  }, {timeout: 3000});
+  userEvent.click(toastLikeButton);
+  // grab the newly added list entry
+  await waitFor(() => {
+    const listEntry = screen.queryByText(submissionFullName);
+    // use special jest-dom matchers like .toBeInTheDocument()
+    expect(listEntry).toBeInTheDocument();
+  }, {timeout: 2000});
+  
 });
